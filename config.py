eb6bf05c9e3e006ebdac5c1a404ba62ca260b78f
ivan <ivanselevickij@gmail.com>
2025-10-29 13:45:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-TOKEN = os.getenv("7894139244:AAFJqI79DoWJhT1xwPJxXDWRrrltn_gZRT8")
+TOKEN = "7894139244:AAFJqI79DoWJhT1xwPJxXDWRrrltn_gZRT8"
